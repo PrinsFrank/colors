30bca04f2825347c1844438b2c3cc454a305143e
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2023-12-07 20:15:36 +0100

    Csfixer

--- tests/Unit/RGB/RGBColorTest.php
+++ tests/Unit/RGB/RGBColorTest.php
@@ -18,19 +18,19 @@ class RGBColorTest extends TestCase
     {
         static::assertEquals((new RGBColor(0, 0, 0))->toHSV(), new HSVColor(0, 0, 0), 'Black');
         static::assertEquals((new RGBColor(255, 255, 255))->toHSV(), new HSVColor(0, 0, 100), 'White');
-        static::assertEquals((new RGBColor(255,0,0))->toHSV(), new HSVColor(0, 100, 100), 'Red');
-        static::assertEquals((new RGBColor(0,255,0))->toHSV(), new HSVColor(120, 100, 100), 'Lime');
-        static::assertEquals((new RGBColor(0,0,255))->toHSV(), new HSVColor(240, 100, 100), 'Blue');
-        static::assertEquals((new RGBColor(255,255,0))->toHSV(), new HSVColor(60, 100, 100), 'Yellow');
-        static::assertEquals((new RGBColor(0,255,255))->toHSV(), new HSVColor(180, 100, 100), 'Cyan');
-        static::assertEquals((new RGBColor(255,0,255))->toHSV(), new HSVColor(300, 100, 100), 'Magenta');
-        static::assertEquals((new RGBColor(191,191,191))->toHSV(), new HSVColor(0, 0, 75), 'Silver');
-        static::assertEquals((new RGBColor(128,128,128))->toHSV(), new HSVColor(0, 0, 50), 'Gray');
-        static::assertEquals((new RGBColor(128,0,0))->toHSV(), new HSVColor(0, 100, 50), 'Maroon');
-        static::assertEquals((new RGBColor(128,128,0))->toHSV(), new HSVColor(60, 100, 50), 'Olive');
-        static::assertEquals((new RGBColor(0,128,0))->toHSV(), new HSVColor(120, 100, 50), 'Green');
-        static::assertEquals((new RGBColor(128,0,128))->toHSV(), new HSVColor(300, 100, 50), 'Purple');
-        static::assertEquals((new RGBColor(0,128,128))->toHSV(), new HSVColor(180, 100, 50), 'Teal');
-        static::assertEquals((new RGBColor(0,0,128))->toHSV(), new HSVColor(240, 100, 50), 'Navy');
+        static::assertEquals((new RGBColor(255, 0, 0))->toHSV(), new HSVColor(0, 100, 100), 'Red');
+        static::assertEquals((new RGBColor(0, 255, 0))->toHSV(), new HSVColor(120, 100, 100), 'Lime');
+        static::assertEquals((new RGBColor(0, 0, 255))->toHSV(), new HSVColor(240, 100, 100), 'Blue');
+        static::assertEquals((new RGBColor(255, 255, 0))->toHSV(), new HSVColor(60, 100, 100), 'Yellow');
+        static::assertEquals((new RGBColor(0, 255, 255))->toHSV(), new HSVColor(180, 100, 100), 'Cyan');
+        static::assertEquals((new RGBColor(255, 0, 255))->toHSV(), new HSVColor(300, 100, 100), 'Magenta');
+        static::assertEquals((new RGBColor(191, 191, 191))->toHSV(), new HSVColor(0, 0, 75), 'Silver');
+        static::assertEquals((new RGBColor(128, 128, 128))->toHSV(), new HSVColor(0, 0, 50), 'Gray');
+        static::assertEquals((new RGBColor(128, 0, 0))->toHSV(), new HSVColor(0, 100, 50), 'Maroon');
+        static::assertEquals((new RGBColor(128, 128, 0))->toHSV(), new HSVColor(60, 100, 50), 'Olive');
+        static::assertEquals((new RGBColor(0, 128, 0))->toHSV(), new HSVColor(120, 100, 50), 'Green');
+        static::assertEquals((new RGBColor(128, 0, 128))->toHSV(), new HSVColor(300, 100, 50), 'Purple');
+        static::assertEquals((new RGBColor(0, 128, 128))->toHSV(), new HSVColor(180, 100, 50), 'Teal');
+        static::assertEquals((new RGBColor(0, 0, 128))->toHSV(), new HSVColor(240, 100, 50), 'Navy');
     }
 }
